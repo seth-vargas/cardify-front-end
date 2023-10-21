24f96400fa02b297d44050d3b3eaeea31289de58
Seth Vargas <shvargas01@gmail.com>
2023-10-20 23:01:48 -0500

    Fixed label not floating

--- src/formComponents/TextAreaInput.jsx
+++ src/formComponents/TextAreaInput.jsx
@@ -2,15 +2,15 @@ import React from "react";
 
 export default function TextAreaInput({ placeholder, name, register }) {
   return (
-    <div className="mb-3">
-      <label htmlFor={name} className="form-label">
-        {placeholder}
-      </label>
+    <div className="mb-3 form-floating">
       <textarea
         className="form-control"
         placeholder={placeholder}
         {...register(name)}
       />
+      <label htmlFor={name} className="form-label">
+        {placeholder}
+      </label>
     </div>
   );
 }
