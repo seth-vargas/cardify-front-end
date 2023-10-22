75a3ad4e347ec3c9d6523c710bdd2775eb070f56
Seth Vargas <shvargas01@gmail.com>
2023-10-22 13:54:47 -0500

    Added comments on all methods; Created methods and todos

--- src/api.js
+++ src/api.js
@@ -3,10 +3,14 @@ import axios from "axios";
 const BASE_API_URL = "http://localhost:3000/api";
 
 export default class CardifyApi {
+  /* Log user in */
+
   static async login(data) {
     console.log(`Logging ${username} in`); // TODO: DELETE BEFORE SUBMITTING
     console.log("data: ", data); // TODO: DELETE BEFORE SUBMITTING
   }
+
+  /* Create new user in db */
 
   static async createUser(data) {
     console.log("Creating new user"); // TODO: DELETE BEFORE SUBMITTING
@@ -16,10 +20,14 @@ export default class CardifyApi {
     return result.data;
   }
 
+  /* Grab user data from db */
+
   static async getUser(username) {
     const result = await axios.get(`${BASE_API_URL}/users/${username}`);
     return result.data;
   }
+
+  /* Create new deck in db */
 
   static async createDeck(data) {
     const result = await axios.post(`${BASE_API_URL}/decks`, data);
@@ -37,6 +45,19 @@ export default class CardifyApi {
     return result.data;
   }
 
+  /* Change deck info in db */
+
+  static async editDeck(data) {
+    console.log(data);
+    console.log("Editing...");
+  }
+
+  /* Remove deck from db using deck ID */
+
+  static async deleteDeck(deckId) {
+    console.log("Deleting...");
+  }
+
   /* Search for decks based on user input
    * Returns list of decks where deckName ILIKE input AND tagName IS input
    */
@@ -47,6 +68,8 @@ export default class CardifyApi {
     );
     return result.data;
   }
+
+  /* Create new flashcard in db */
 
   static async createFlashcard(data) {
     console.log("Creating new flashcard"); // TODO: DELETE BEFORE SUBMITTING
@@ -59,6 +82,8 @@ export default class CardifyApi {
 
     return result.data;
   }
+
+  /* Get list of cards from db */
 
   static async getCards(username, deckSlug) {
     const result = await axios.get(
