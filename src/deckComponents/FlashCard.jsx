311226610e9c3038e9fe8f31c1431c7556e322bf
Seth Vargas <shvargas01@gmail.com>
2023-10-24 23:03:28 -0500

    Flashcards now function as expected

--- src/deckComponents/FlashCard.jsx
+++ src/deckComponents/FlashCard.jsx
@@ -1,13 +1,49 @@
 /* FlashCard: Renders a card with relevant information from a users deck */
 
-import React from "react";
+import { useState, useEffect, useRef } from "react";
+import "./FlashCard.css";
 
 export default function FlashCard({ front, back }) {
+  // return (
+  //   <div className="card shadow-sm col-6 m-2">
+  //     <div className="card-body">
+  //       <h5 className="card-title">{front}</h5>
+  //       <p>{back}</p>
+  //     </div>
+  //   </div>
+  // );
+  const [flip, setFlip] = useState(false);
+  const [height, setHeight] = useState("initial");
+
+  const frontEl = useRef();
+  const backEl = useRef();
+
+  function setMaxHeight() {
+    const frontHeight = frontEl.current.getBoundingClientRect().height;
+    const backHeight = backEl.current.getBoundingClientRect().height;
+    setHeight(Math.max(frontHeight, backHeight, 200));
+  }
+
+  useEffect(setMaxHeight, [front, back]);
+
+  useEffect(() => {
+    window.addEventListener("resize", setMaxHeight);
+    return () => window.removeEventListener("resize", setMaxHeight);
+  }, []);
+
   return (
-    <div className="card me-3 mb-3" style={{ height: "15rem" }}>
-      <div className="card-body">
-        <h5 className="card-title">{front}</h5>
-        <p>{back}</p>
+    <div
+      className={`card ${
+        flip ? "flip" : ""
+      }  col-lg-4 col-md-8 row text-center`}
+      style={{ height: height }}
+      onClick={() => setFlip(!flip)}
+    >
+      <div className="front" ref={frontEl}>
+        {front}
+      </div>
+      <div className="back" ref={backEl}>
+        {back}
       </div>
     </div>
   );
--- src/deckComponents/FlashCardList.jsx
+++ src/deckComponents/FlashCardList.jsx
@@ -3,20 +3,22 @@ import FlashCard from "./FlashCard";
 
 export default function FlashCardList({ title, cards, username, slug }) {
   return (
-    <div>
+    <div className="row">
       <h4 className="mt-5">{title}</h4>
-      <div className="row my-3">
+      <div className="row justify-content-center">
         <a
           data-bs-toggle="modal"
           data-bs-target="#createFlashcardModal"
-          className="btn btn-outline-success"
+          className="btn btn-outline-success col-lg-4 col-md-8"
         >
           Create a new flashcard!
         </a>
       </div>
-      <div className="row">
+      <div className="row justify-content-center my-3">
         {cards.map((card) => (
-          <FlashCard front={card.front} back={card.back} key={card.id} />
+          <div className="row justify-content-center my-3" key={card.id}>
+            <FlashCard front={card.front} back={card.back} />
+          </div>
         ))}
       </div>
     </div>
