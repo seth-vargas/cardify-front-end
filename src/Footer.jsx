5737ce00934f100b7f37de6df9727608fea5b575
Seth Vargas <shvargas01@gmail.com>
2023-11-22 22:16:47 -0600

    Added footer

---
+++ src/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer className="fixed-bottom">
+      <hr />
+      <p className="text-center text-muted">© 2023 Seth Vargas</p>
+    </footer>
+  );
+}
